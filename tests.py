80d3adc4e86da22e476c203f79afda932485cc5a
Mihir Singh <me@mihirsingh.com>
2014-01-13 22:57:00 -0500

    Basic unit tests

---
+++ tests.py
@@ -0,0 +1,35 @@
+import unittest
+from musketeers import Musketeers
+
+def test_get_verb():
+
+	client = Musketeers()
+	client.use("httpbin")
+
+	params = {
+		"name": "Bruce Wayne",
+		"age": 18
+	}
+
+	response = client.post("post", params=params).json()
+
+	assert(response['args']['name'] == "Bruce Wayne")
+
+def test_post_verb():
+
+	client = Musketeers()
+	client.use("httpbin")
+
+	form = {
+		"name": "Bruce Wayne",
+		"age": 18
+	}
+
+	response = client.post("post", form=form).json()
+
+	assert(response['form']['name'] == "Bruce Wayne")
+
+if __name__ == "__main__":
+
+	test_post_verb()
+	test_get_verb()
