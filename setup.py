b1aae742c9b8be7ec10019019f685b7e43b66cc0
Mihir Singh <me@mihirsingh.com>
2014-01-14 09:34:05 -0500

    Installation via setup.py install

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+long_description = \
+'''Github_.
+
+.. _Github: http://github.com/citruspi/Musketeers
+'''
+
+setup(
+    name='Musketeers',
+    version='0.1.0',
+    author='Mihir Singh',
+    author_email='me@mihirsingh.com',
+    packages=['musketeers'],
+    url='http://pypi.python.org/pypi/musketeers/',
+    license='MIT License',
+    description='All for one, one for all!',
+    long_description=long_description,
+    install_requires=[
+                        'requests'
+                     ],
+    classifiers=[
+                    'Development Status :: 3 - Alpha',
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: MIT License',
+                    'Programming Language :: Python',
+                    'Topic :: System :: Networking',
+                    'Topic :: Internet'
+                ]
+)
